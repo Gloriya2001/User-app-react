007e09338375e39f1b366656020d99015fbd4458
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-12 06:45:48 +0530

    input read of signup form done to console

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useInRouterContext } from 'react-router-dom'
 
 const SignUp = () => {
 
-        
+    const [data, setData] = useState(
 
+        {
+            "name": "",
+            "dob": "",
+            "bloodgp": "",
+            "mobNum": "",
+            "address": "",
+            "pin": "",
+            "district": "",
+            "place": "",
+            "email": "",
+            "userName": "",
+            "password": ""
+        }
+
+    )
+
+    const inputHandler = (event) => {
+
+        setData({ ...data, [event.target.name]: event.target.value })
+
+    }
+
+    const readValue = () => {
+
+        console.log(data)
+
+    }
 
 
 
@@ -18,21 +46,21 @@ const SignUp = () => {
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Name</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler}/>
 
                             </div>
 
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Date of birth</label>
-                                <input type="date" className="form-control" />
+                                <input type="date" className="form-control" name='dob' value={data.dob} onChange={inputHandler}/>
 
                             </div>
 
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Blood Group</label>
-                                <select name="" id="" className="form-control">
+                                <select  id="" className="form-control" name='bloodgp' value={data.bloodgp} onChange={inputHandler}>
                                     <option value="A+">A+</option>
                                     <option value="B+">B+</option>
                                 </select>
@@ -43,28 +71,28 @@ const SignUp = () => {
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Mobile Number</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='mobNum' value={data.mobNum} onChange={inputHandler} />
 
                             </div>
 
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                                 <label htmlFor="" className="form-label">Address</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='address' value={data.address} onChange={inputHandler}/>
 
                             </div>
 
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">PIN CODE</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='pin' value={data.pin} onChange={inputHandler}/>
 
                             </div>
 
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">District</label>
-                                <select name="" id="" className="form-control">
+                                <select  id="" className="form-control" name='district' value={data.district} onChange={useInRouterContext} >
                                     <option value="Thrissur">Thrissur</option>
                                     <option value="Ernankulam">Ernankulam</option>
                                     <option value="Wayanad">Wayanad</option>
@@ -74,28 +102,28 @@ const SignUp = () => {
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">place</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='place' value={data.place} onChange={inputHandler}/>
 
                             </div>
 
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Email Id</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='email' value={data.email} onChange={inputHandler} />
 
                             </div>
 
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                                 <label htmlFor="" className="form-label">User Name</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='userName' value={data.userName} onChange={inputHandler}/>
 
                             </div>
 
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
                                 <label htmlFor="" className="form-label">Password</label>
-                                <input type="Password" className="form-control" />
+                                <input type="Password" className="form-control" name='password' value={data.password} onChange={inputHandler}/>
 
                             </div>
 
@@ -109,7 +137,7 @@ const SignUp = () => {
 
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                             <button className="btn btn-success">Register</button>
+                                <button className="btn btn-success" onClick={readValue}>Register</button>
 
                             </div>
 
